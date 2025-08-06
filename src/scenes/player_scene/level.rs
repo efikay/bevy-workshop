55a668f1559025d48873dca5f2a7f359af15b7ed
Efi Kay <efikay@icloud.com>
2025-08-06 12:39:54 +0300

    Remove lots of comments and unused junk
    In case I need examples: there's always bevy_new_2d repo to come back and take a peek

--- src/scenes/player_scene/level.rs
+++ src/scenes/player_scene/level.rs
@@ -1,5 +1,3 @@
-//! Spawn the main level.
-
 use bevy::prelude::*;
 
 use crate::{
@@ -10,34 +8,10 @@ use crate::{
 };
 
 pub(super) fn plugin(_: &mut App) {
-    // app.register_type::<LevelAssets>();
-    // app.load_resource::<LevelAssets>();
+    //
 }
 
-// #[derive(Resource, Asset, Clone, Reflect)]
-// #[reflect(Resource)]
-// pub struct LevelAssets {
-//     #[dependency]
-//     music: Handle<AudioSource>,
-// }
-
-// impl FromWorld for LevelAssets {
-//     fn from_world(world: &mut World) -> Self {
-//         let assets = world.resource::<AssetServer>();
-//         Self {
-//             music: assets.load("audio/music/Fluffing A Duck.ogg"),
-//         }
-//     }
-// }
-
-/// A system that spawns the main level.
-pub fn spawn_level(
-    mut commands: Commands,
-    // level_assets: Res<LevelAssets>,
-    // player_assets: Res<PlayerAssets>,
-    asset_server: Res<AssetServer>,
-    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
-) {
+pub fn spawn_level(mut commands: Commands) {
     for sprite_bundle in
         chess_floor::bundles::make_sprite_bundles(chess_floor::config::ChessFloorConfig {
             area: Rect::from_center_size(Vec2::ZERO, Vec2::new(2000.0, 2000.0)),
@@ -69,9 +43,3 @@ pub fn spawn_level(
         StateScoped(ScreenState::Gameplay),
     ));
 }
-
-// player(400.0, &player_assets, &mut texture_atlas_layouts),
-// (
-// Name::new("Gameplay Music"),
-// music(level_assets.music.clone())
-// )
--- src/scenes/player_scene/mod.rs
+++ src/scenes/player_scene/mod.rs
@@ -1,8 +1,3 @@
-//! Demo gameplay. All of these modules are only intended for demonstration
-//! purposes and should be replaced with your own game logic.
-//! Feel free to change the logic found here if you feel like tinkering around
-//! to get a feeling for the template.
-
 use bevy::prelude::*;
 
 use crate::{components::screen_wrap, features::creature};
@@ -11,6 +6,5 @@ pub mod level;
 
 pub(super) fn plugin(app: &mut App) {
     app.add_plugins((screen_wrap::plugin, creature::plugin, level::plugin));
-
     // app.add_plugins(audio_mixer::plugin);
 }
