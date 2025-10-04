f764e6392caf2eea937af379d7a3c47e14cf11e2
Efi Kay <efikay@icloud.com>
2025-10-04 20:36:19 +0300

    Remove duplicate init_state

--- src/app/base_plugins.rs
+++ src/app/base_plugins.rs
@@ -23,7 +23,6 @@ impl Plugin for BasePlugins {
 
         // Set up the `Pause` state.
         app.init_state::<AppPauseState>();
-        app.init_state::<AppPauseState>();
         app.configure_sets(
             Update,
             PausableAppSystems.run_if(in_state(AppPauseState(false))),
